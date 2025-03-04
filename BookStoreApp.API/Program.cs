c3432da25a88e77291ff0f1a5d1a8484dda7e016
rocko jones <eleven11.tv@outlook.com>
2025-03-04 11:43:06 -0800

    aftLect14: connexion string elements added, nuget ms efcore sql, tools, design pkgs, scaffolded dbase to Data dir that holds the model et dbcontext classes #

--- BookStoreApp.API/Program.cs
+++ BookStoreApp.API/Program.cs
@@ -1,8 +1,14 @@
-using Serilog; 
+using Serilog;
+using Microsoft.EntityFrameworkCore;
+using BookStoreApp.API.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connString = builder.Configuration.GetConnectionString("BookStoreAppDbConnection");
+builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseSqlServer(connString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
